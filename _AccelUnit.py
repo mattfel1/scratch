1b4816cf03522dbed83d461a8e2e62d57d1eca82
mattfel <feldman.matthew1@gmail.com>
2019-10-18 21:42:54 -0700

    new app

--- _AccelUnit.py
+++ _AccelUnit.py
@@ -40,55 +40,23 @@ class AccelUnit(pr.Device,rogue.interfaces.stream.Slave):
 ##### ArgIns
 
 ##### DRAM Ptrs:
-        self.add(pr.RemoteVariable(name = 'IN_ptr', description = 'dram ptr', offset = 8, bitSize = 32, bitOffset = 0, mode = 'RW',))
+        self.add(pr.RemoteVariable(name = 'OUT_ptr', description = 'dram ptr', offset = 8, bitSize = 32, bitOffset = 0, mode = 'RW',))
 
 ##### ArgIOs
 
 ##### ArgOuts
-        self.add(pr.RemoteVariable(name = 'X418_arg', description = 'argIn', offset = 12, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X419_arg', description = 'argIn', offset = 16, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X420_arg', description = 'argIn', offset = 20, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X421_arg', description = 'argIn', offset = 24, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X422_arg', description = 'argIn', offset = 28, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X423_arg', description = 'argIn', offset = 32, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X424_arg', description = 'argIn', offset = 36, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X425_arg', description = 'argIn', offset = 40, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X426_arg', description = 'argIn', offset = 44, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X427_arg', description = 'argIn', offset = 48, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X428_arg', description = 'argIn', offset = 52, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X429_arg', description = 'argIn', offset = 56, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X430_arg', description = 'argIn', offset = 60, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X431_arg', description = 'argIn', offset = 64, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X432_arg', description = 'argIn', offset = 68, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X433_arg', description = 'argIn', offset = 72, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X434_arg', description = 'argIn', offset = 76, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X435_arg', description = 'argIn', offset = 80, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X436_arg', description = 'argIn', offset = 84, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X437_arg', description = 'argIn', offset = 88, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X438_arg', description = 'argIn', offset = 92, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X439_arg', description = 'argIn', offset = 96, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X440_arg', description = 'argIn', offset = 100, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X441_arg', description = 'argIn', offset = 104, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X442_arg', description = 'argIn', offset = 108, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X443_arg', description = 'argIn', offset = 112, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X444_arg', description = 'argIn', offset = 116, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X445_arg', description = 'argIn', offset = 120, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X446_arg', description = 'argIn', offset = 124, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X447_arg', description = 'argIn', offset = 128, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X448_arg', description = 'argIn', offset = 132, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X449_arg', description = 'argIn', offset = 136, bitSize = 32, bitOffset = 0, mode = 'RO',))
 
 ##### Instrumentation Counters
-        self.add(pr.RemoteVariable(name = 'X119_cycles_arg', description = 'cycs', offset = 140, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X119_iters_arg', description = 'numiters', offset = 144, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X463_cycles_arg', description = 'cycs', offset = 148, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X463_iters_arg', description = 'numiters', offset = 152, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X462_cycles_arg', description = 'cycs', offset = 156, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X462_iters_arg', description = 'numiters', offset = 160, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X462_stalled_arg', description = 'stalled', offset = 164, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X462_idle_arg', description = 'idle', offset = 168, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X552_cycles_arg', description = 'cycs', offset = 172, bitSize = 32, bitOffset = 0, mode = 'RO',))
-        self.add(pr.RemoteVariable(name = 'X552_iters_arg', description = 'numiters', offset = 176, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X20_cycles_arg', description = 'cycs', offset = 12, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X20_iters_arg', description = 'numiters', offset = 16, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X96_cycles_arg', description = 'cycs', offset = 20, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X96_iters_arg', description = 'numiters', offset = 24, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X107_cycles_arg', description = 'cycs', offset = 28, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X107_iters_arg', description = 'numiters', offset = 32, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X106_cycles_arg', description = 'cycs', offset = 36, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X106_iters_arg', description = 'numiters', offset = 40, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X106_stalled_arg', description = 'stalled', offset = 44, bitSize = 32, bitOffset = 0, mode = 'RO',))
+        self.add(pr.RemoteVariable(name = 'X106_idle_arg', description = 'idle', offset = 48, bitSize = 32, bitOffset = 0, mode = 'RO',))
 
 ##### Early Exits
     # Unused code for testing how to receive a frame in SW
