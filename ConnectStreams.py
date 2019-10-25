f58db69e2e1b27879bb7e40041e223bc70d780f9
mattfel <feldman.matthew1@gmail.com>
2019-10-24 17:06:57 -0700

    new app

--- ConnectStreams.py
+++ ConnectStreams.py
@@ -1,6 +1,29 @@
-base.x90_port = rogue.interfaces.stream.TcpClient('localhost', 8000 + (0+1)*2 + 0 * 512)
-base.x91_frame = FrameContainer()
-pyrogue.streamConnect(base.x91_frame, base.x1_port)
-base.x95_port = rogue.interfaces.stream.TcpClient('localhost', 8000 + (1+1)*2 + 0 * 512)
-base.x96_frame = FrameContainer()
-pyrogue.streamConnect(base.x96_frame, base.x7_port)
+#!/usr/bin/env python3
+import setupLibPaths
+
+import sys
+import argparse
+import rogue
+import rogue.hardware.axi
+import rogue.interfaces.stream
+import rogue.interfaces.memory
+
+import pyrogue as pr
+import pyrogue.gui
+import pyrogue.utilities.prbs
+import pyrogue.interfaces.simulation
+
+import axipcie  as pcie
+# import rogue.axi as axi
+
+import time
+from FrameContainer import FrameContainer
+
+def connect(base):
+  base.x90_port = rogue.interfaces.stream.TcpClient('localhost', 8000 + (0+1)*2 + 0 * 512)
+  base.x91_frame = FrameContainer()
+  pyrogue.streamConnect(base.x91_frame, base.x1_port)
+  base.x95_port = rogue.interfaces.stream.TcpClient('localhost', 8000 + (1+1)*2 + 0 * 512)
+  base.x96_frame = FrameContainer()
+  pyrogue.streamConnect(base.x96_frame, base.x7_port)
+
