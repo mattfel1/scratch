3d66cb3bdfc0133238c907aad80070b94d6a0425
mattfel <feldman.matthew1@gmail.com>
2019-10-24 20:07:30 -0700

    new app

--- ConnectStreams.py
+++ ConnectStreams.py
@@ -22,8 +22,8 @@ from FrameContainer import FrameContainer
 def connect(base):
   base.x90_port = rogue.interfaces.stream.TcpClient('localhost', 8000 + (0+1)*2 + 0 * 512)
   base.x91_frame = FrameContainer()
-  pyrogue.streamConnect(base.x91_frame, base.x1_port)
+  pyrogue.streamConnect(base.x91_frame, base.x90_port)
   base.x95_port = rogue.interfaces.stream.TcpClient('localhost', 8000 + (1+1)*2 + 0 * 512)
   base.x96_frame = FrameContainer()
-  pyrogue.streamConnect(base.x96_frame, base.x7_port)
+  pyrogue.streamConnect(base.x96_frame, base.x95_port)
 
